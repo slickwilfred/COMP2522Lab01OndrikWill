7cbf1f5ed0c2cb0cff56d7deb13ecceaeba186cc
Will Ondrik <willondrik@icloud.com>
2023-01-04 16:54:31 -0800

    Fixed original checkstyle errors and added a unique "hello world" message

---
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -0,0 +1,14 @@
+package org.bcit.comp2522.labs.lab01;
+
+/**
+ * This program prints a unique message and image.
+ *
+ */
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Hello world! My name is Will Ondrik.");
+  }
+
+
+}
